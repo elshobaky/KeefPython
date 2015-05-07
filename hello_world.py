c1c263f28e35eab6b1ca119d0292d35761a43ca0
Mahmoud Elshobaky <mahmoud.elshobaky@gmail.com>
2015-05-07 11:22:10 -0700

    Add 1st tutorial file

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+print "Hello, World!"
+#>>>"hello"
+def say_hi():
+	output = "Welcome to KeefCode\n"
+	output += "after this tutorial :\n"
+	output += "you'll get python installed on your PC\n"
+	output += "and you'll create your first hell world app!"
+	#print output
+	return output
+
+print say_hi()
