0b07cb3c75adec5349d29a8d72024a501b9ba7e4
Mahmoud Elshobaky <mahmoud.elshobaky@gmail.com>
2015-05-07 11:22:45 -0700

    Add 2nd tutorial file

---
+++ python_syntax.py
@@ -0,0 +1,26 @@
+# assigning string to a variable name
+website = "KeefCode"
+# indentations rules
+if website == "KeefCode" : # this is the heading
+    # this is the suite
+    print "Welcome!"
+    print "you're  in the right place!"
+else : 
+  print "I think you should visit KeefCode, I'm sure you'll like it"
+
+# writing two statements in the same line
+print "Hello, World!" ; print "welcome to KeefCode"
+
+# Multi-Line Statement
+print 1 + 2 \
+    + 3 + 4 \
+    + 5 + 6
+
+# Quotaions
+single_quoted = 'Hi, I am a single quoted string'
+double_quoted = "Hi, I'm a double quoted string"
+triple_quoted = """ Hi, 
+I'm a multi-line string """
+
+# these words are skipped by the interpreter 
+# as it is prefixed by #
