d41cf579077495ef99d8c2667d26858bdc7e2ae4
Mahmoud Elshobaky <mahmoud.elshobaky@gmail.com>
2015-05-09 18:23:50 -0700

    Add file for 3rd tutorial "calculating"

---
+++ calculating with python.py
@@ -0,0 +1,60 @@
+
+# you can use these operators to perform calculation
+# (+) for adding         (-) for subtracting
+# (*) for multiplying    (/) for division but wait ...
+# (**) for power         (//) for division to get integer result..
+
+####################################
+# Programming is Fun with KeefCode #
+####################################
+
+# Adding
+5+3+4
+5 + 3 + 4 # you can use spaces for organization but it's optional
+print 5+3+4  # to view the answer run the code in IDLE or use print
+#>>> 12
+
+# Subtraction
+8-5 ; 190 - 53
+print 180 - 160
+#>>> 20
+
+# Multiplying
+8*5*2
+print 10*15
+#>>> 150
+
+# operation with multiple operators
+print 10 - 2 * 4 * 5 / 8 + 15
+#>>> 20
+
+# Division in python 2.
+print 8/4 #>>> 2
+print 10/4 #>>> 2
+# use . to convert int to float
+print 10./4 #>>> 2.5
+print 8./4 #>>> 2.0
+# use % to get the ramainder
+print 10 % 4  #>>> 2
+print 8 % 4 #>>> 0
+
+# Division in python 3.
+print(8/4) #>>>2.0
+print(10/4) #>>> 2.5
+
+# use // to get the result as an int
+# the // operator is also available in python 2
+print(10//4) #>>> 2
+
+# calculate the power
+print 5**2 #>>> 25
+print 2**3 #>>> 8
+
+# using math module
+import math
+# calculate square root
+print math.sqrt(25) #>>> 5
+# calculate factorial
+print math.factorial(5) #>>> 1*1*2*3*4*5 = 120
+
+# Division 
