550e78c2d65b97f70efc0554219ce46d739c2ccf
Mahmoud Elshobaky <mahmoud.elshobaky@gmail.com>
2015-05-13 14:37:59 +0200

    Add file for 4th tutorial "variables"

---
+++ variables.py
@@ -0,0 +1,28 @@
+# assigning values to variables
+new_var = 15
+
+# calculating perimeter and area of a rectangle
+legnth = 10
+width = 5
+
+perimeter = (legnth + width) * 2
+area = legnth * width
+
+print "The rectangle perimeter = " , perimeter
+print "The rectangle area = " , area
+
+# changing variable Value
+
+new_var = 20
+print "new_var current value is ", new_var
+new_var = "KeefCode"
+print "new_var value changed to ", new_var
+
+# multiple assignment
+var_a = var_b = var_c = var_d = 20
+print "var_a equals " , var_a
+print "var_c equals " , var_c
+
+var_a , var_b , var_c , var_d = 10 , 15 , 20 , 25
+print "var_a equals " , var_a
+print "var_c equals " , var_c
